0d3a78dcde70bdbc03f655f9bb3dad303b79290b
Xavier <Xavier@DESKTOP-7AMJ3O3>
2024-09-08 23:11:59 -0500

    Agregar archivos de proyecto.

---
+++ Models/CiudadesModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using _06Publicaciones.config;
+using System.Data.SqlClient;
+
+namespace _06Publicaciones.Models
+{
+    
+    class CiudadesModel
+    {
+        
+        public int IdCiudad { get; set; }
+        public string Detalle { get; set; }
+        public int idPais { get; set; }
+
+        public DataTable todosconrelacion() {
+            var cadena = "SELECT Ciudades.IdCiudad, Ciudades.Detalle as Ciudad, Paises.IdPais, Paises.Detalle AS 'Pais' FROM Ciudades INNER JOIN Paises ON Ciudades.idPais = Paises.IdPais";
+            using (var cn = Conexion.GetConnection()) {
+                try
+                {
+                    SqlDataAdapter adaptador = new SqlDataAdapter(cadena, cn);
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    return tabla;
+                }
+                catch (SqlException ex)
+                {
+                    ErrorHandler.ManejarErrorSql(ex, "Error al insertar el autor.");
+                }
+                catch (Exception ex)
+                {
+                    ErrorHandler.ManejarErrorGeneral(ex, "Error al insertar el autor.");
+                }
+                return null;
+            }      
+        }
+    }
+
+    //crud
+    
+    
+
+}
---
+++ Views/Ciudades/frm_Ciudades.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _06Publicaciones.Controllers;
+namespace _06Publicaciones.Views.Ciudades
+{
+    public partial class frm_Ciudades : Form
+    {
+        private string id;
+        public frm_Ciudades(string id )
+        {
+            InitializeComponent();
+            this.id = id;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_Ciudades_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.id);
+            PaisesController _paises = new PaisesController();
+            comboBox1.DataSource = _paises.todos();
+            comboBox1.DisplayMember = "Detalle";
+            comboBox1.ValueMember = "IdPais";
+            
+
+        }
+    }
+}
+
